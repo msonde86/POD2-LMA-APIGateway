f7b11ec35788c9ed79f26255bf0a9bc9b599f315
unknown <ctsjavauser298@iiht.tech>
2020-09-03 13:08:30 +0530

    sprint 2 adding zuul pre filter

---
+++ Zuul-Api-gateway/src/main/java/com/scb/pod2/zuul/filter/ZuulPreFilter.java
@@ -0,0 +1,52 @@
+package com.scb.pod2.zuul.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+public class ZuulPreFilter extends ZuulFilter{
+
+	private Logger logger = LoggerFactory.getLogger(ZuulPreFilter.class);
+	@Override
+	public boolean shouldFilter() {
+		RequestContext context = RequestContext.getCurrentContext();
+		HttpServletRequest request = context.getRequest();
+		if(request.getRequestURL().toString().equals("http://localhost:8084/login-service/api/login/authenticate")){
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		logger.info("Executing zuul pre filter");
+		RequestContext context = RequestContext.getCurrentContext();
+		HttpServletRequest request = context.getRequest();
+		String authHeader = request.getHeader("authorization");
+		if(authHeader == null || authHeader.trim().equals("") || !authHeader.startsWith("Bearer")) {
+			logger.error("Zuul filter: Invalid token");
+			context.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+			context.setResponseBody("Access denied. Invalid token");
+			context.setSendZuulResponse(false);
+		}
+		
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+}
