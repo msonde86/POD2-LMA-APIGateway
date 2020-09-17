6fd375df1c9a921387fad89576937b3c035795a2
unknown <ctsjavauser279@iiht.tech>
2020-09-17 12:54:15 +0530

    commit

--- Zuul-Api-gateway/src/main/java/com/scb/pod2/zuul/config/BeanConfig.java
+++ Zuul-Api-gateway/src/main/java/com/scb/pod2/zuul/config/BeanConfig.java
@@ -4,14 +4,13 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.codec.ServerCodecConfigurer;
 
 @Configuration
 public class BeanConfig {
 
 	@Bean
 	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
-		return builder.routes().route(r -> r.path("/loan/data")
+		return builder.routes().route(r -> r.path("/loan/data/")
 				.filters(f -> f.hystrix(h -> h.setName("Hystrix").setFallbackUri("forward:/fallback/loan/data")))
 				.uri("lb://LOAN-SEARCH-SERVICE").id("LoansearchModule"))
 
